7f97af40e83661c1397b182569c32bc5c192aec3
hata-tomoyuki <h.tomo0602@gmail.com>
2019-12-21 13:01:42 +0900

    commit1
    add function for auto-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,42 @@
 $(function(){
-  function buildHTML(message){
+  var buildHTML = function(message) {
     if (message.image) {
-      var html = `<div class="message">
-                    <div class="upper-message"> 
-                      <div class="upper-message__name">
-                        ${message.name}
-                      </div>
-                      <div class="upper-message__time">
-                      ${message.date}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${message.text}
-                      </p>
-                      <img class="lower-message__image">
-                      <img class="lower-message__image", src=${message.image}>
-                    </div>
-                  </div>`
+      var html =  `<div class="message" data-message-id=` + message.id + `>` +
+                    `<div class="upper-message">` +
+                      `<div class="upper-message__name">` +
+                        message.name +
+                      `</div>` +
+                      `<div class="upper-message__time">` +
+                        message.date +
+                      `</div>` +
+                    `</div>` +
+                    `<div class="lower-message">` +
+                      `<p class="lower-message__content">` +
+                        message.text +
+                      `</p>` +
+                      `<img class="lower-message__image">` +
+                      `<img class="lower-message__image", src=` + message.image + `>` +
+                    `</div>` +
+                  `</div>`
     } else {
-      var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__name">
-                        ${message.name}
-                      </div>
-                      <div class="upper-message__time">
-                      ${message.date}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${message.text}
-                      </p>
-                    </div>
-                  </div>`
-    }
+      var html =  `<div class="message" data-message-id=` + message.id + `>` +
+                    `<div class="upper-message">` +
+                      `<div class="upper-message__name">` +
+                        message.name +
+                      `</div>` +
+                      `<div class="upper-message__time">` +
+                        message.date +
+                      `</div>` +
+                    `</div>` +
+                    `<div class="lower-message">` +
+                      `<p class="lower-message__content">` +
+                        message.text +
+                      `</p>` +
+                    `</div>` +
+                  `</div>`
+    };
     return html;
-  }
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -61,4 +61,27 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   })
-})
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
 json.text @message.text
 json.name @message.user.name
 json.image @message.image.url
-json.date @message.created_at.strftime("%Y年%-m月%-d日 %-H時%-M分")
+json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.id @message.id
